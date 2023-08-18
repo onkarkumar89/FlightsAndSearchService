64db96e20afafec076049bb4a832c88b29775555
onkarkumar <okrajsingh@gmail.com>
2023-08-19 00:29:25 +0530

    Added more routes for city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,7 @@ const CityController = require("../../controllers/city-controller");
 
 const router = express.Router();
 router.post("/city", CityController.create);
-//route.delete("/city/:id", CityController.destroy);
-
+route.delete("/city/:id", CityController.destroy);
+route.get("/city/:id", CityController.get);
+route.patch("/city/:id", CityController.update);
 module.exports = router;
