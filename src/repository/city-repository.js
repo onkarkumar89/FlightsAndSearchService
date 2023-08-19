51f5a5a23901e0f3895602843b7931d361916a61
onkarkumar <okrajsingh@gmail.com>
2023-08-19 14:21:23 +0530

    minor fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -21,7 +21,17 @@ class CityRepository {
   }
   async updateCity(cityId, data) {
     try {
-      const city = await City.update(data, { where: { id: cityId } });
+      // The below approach also work but will not return upated object
+      // if we are using pg then returning : true can be used else not
+      // const city = await City.update(data, {
+      //   where: { id: cityId },
+      //   returning: true,
+      //   plain: true,
+      // });
+      // for getting upated data in mysql we use the below approch
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
       return city;
     } catch (error) {
       console.log("Something went wrong in the repository layer");
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,7 +3,7 @@ const CityController = require("../../controllers/city-controller");
 
 const router = express.Router();
 router.post("/city", CityController.create);
-route.delete("/city/:id", CityController.destroy);
-route.get("/city/:id", CityController.get);
-route.patch("/city/:id", CityController.update);
+router.delete("/city/:id", CityController.destroy);
+router.get("/city/:id", CityController.get);
+router.patch("/city/:id", CityController.update);
 module.exports = router;
